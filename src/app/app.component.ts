6be06fae6d82f78afb1d361e24ecb350c2e96096
Stephen Oloto <steevyn51@gmail.com>
2021-03-02 02:08:33 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'filed-test';
+  optionForm : FormGroup;
+
+  
+  ngOnInit(): void {
+    this.optionForm = new FormGroup({
+      option: new FormControl('', Validators.required),
+      adBudget: new FormControl('', Validators.required)});
+  }
+
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  next() {
+
+  }
 }
